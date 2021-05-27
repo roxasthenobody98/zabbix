4ea6cc25efeb73eb0480ffe3706ad055640fd2f3
Janis Freibergs <janis.freibergs@zabbix.com>
2021-05-27 15:32:38 +0300

    A......... [ZBX-19107] fixed user role tests

--- ui/tests/api_json/testUserRole.php
+++ ui/tests/api_json/testUserRole.php
@@ -28,32 +28,41 @@ class testUserRole extends CAPITest {
 
 	public static function getUserRoleCreateData() {
 		return [
-			[
+			'Missing type param' => [
 				'role' => [
-					'name' => "Random",
+					[
+						'name' => "Random",
+					],
 				],
 				'expected_error' => 'Invalid parameter "/1": the parameter "type" is missing.'
 			],
-			[
+			'Missing name param' => [
 				'role' => [
-					'type' => USER_TYPE_ZABBIX_USER
+					[
+						'type' => USER_TYPE_ZABBIX_USER
+					]
 				],
 				'expected_error' => 'Invalid parameter "/1": the parameter "name" is missing.'
 			],
-			[
+			'Empty name' => [
 				'role' => [
-					'name' => '',
-					'type' => USER_TYPE_ZABBIX_USER
+					[
+						'name' => '',
+						'type' => USER_TYPE_ZABBIX_USER
+					]
 				],
 				'expected_error' => 'Invalid parameter "/1/name": cannot be empty.'
 			],
-			[
+			'Existing name' => [
 				'role' => [
-					'name' => 'Admin role',
+					[
+						'name' => 'Admin role',
+						'type' => USER_TYPE_ZABBIX_ADMIN
+					]
 				],
 				'expected_error' => 'User role with name "Admin role" already exists.'
 			],
-			[
+			'Existing name in set' => [
 				'role' => [
 					[
 						'name' => 'One user role with existing name',
@@ -66,60 +75,59 @@ class testUserRole extends CAPITest {
 				],
 				'expected_error' => 'User role with name "Admin role" already exists.'
 			],
-			[
+			'Same name in set' => [
 				'role' => [
 					[
 						'name' => 'User role with identical name',
-						'type' => USER_TYPE_ZABBIX_USER,
+						'type' => USER_TYPE_ZABBIX_USER
 					],
 					[
 						'name' => 'User role with identical name',
-						'type' => USER_TYPE_ZABBIX_USER,
+						'type' => USER_TYPE_ZABBIX_USER
 					]
 				],
 				'expected_error' => 'Invalid parameter "/2": value (name)=(User role with identical name) already exists.'
 			],
-			// Check successfully creation of user roles
-			[
+			'Set with UTF' => [
 				'role' => [
 					[
 						'name' => 'User role created via API as part of batch',
-						'type' => USER_TYPE_ZABBIX_USER,
+						'type' => USER_TYPE_ZABBIX_USER
 					],
 					[
 						'name' => 'æų',
-						'type' => USER_TYPE_ZABBIX_USER,
+						'type' => USER_TYPE_ZABBIX_USER
 					]
 				],
 				'expected_error' => null
 			],
-			[
+			'Single user, UTF' => [
 				'role' => [
 					[
 						'name' => 'Апи роль УТФ-8',
-						'type' => USER_TYPE_ZABBIX_USER,
+						'type' => USER_TYPE_ZABBIX_USER
 					]
 				],
 				'expected_error' => null
 			],
-			[
+			'Single Admin' => [
 				'role' => [
 					[
 						'name' => 'User role=admin created via API',
-						'type' => USER_TYPE_ZABBIX_ADMIN,
+						'type' => USER_TYPE_ZABBIX_ADMIN
 					]
 				],
 				'expected_error' => null
 			],
-			[
+			'Single Superadmin' => [
 				'role' => [
 					[
 						'name' => 'User role=superadmin created via API',
-						'type' => USER_TYPE_SUPER_ADMIN,
+						'type' => USER_TYPE_SUPER_ADMIN
 					]
 				],
 				'expected_error' => null
-			]						
+			]
 		];
 	}
 
@@ -129,11 +137,17 @@ class testUserRole extends CAPITest {
 	public function testUserRole_Create($role, $expected_error) {
 		$result = $this->call('role.create', $role, $expected_error);
 
-		if ($expected_error === null) {
-			foreach ($result['result']['roleids'] as $key => $id) {
-				$dbResult = DBSelect('SELECT * FROM role WHERE roleid='.zbx_dbstr($id));
-				$dbRow = DBFetch($dbResult);
+		if ($expected_error !== null) {
+			return;
+		}
+		foreach ($result['result']['roleids'] as $key => $id) {
+			$dbResult = DBSelect('SELECT * FROM role WHERE roleid='.zbx_dbstr($id));
+			$dbRow = DBFetch($dbResult);
+
+			if (array_key_exists('name', $role[$key])) {
 				$this->assertEquals($dbRow['name'], $role[$key]['name']);
+			}
+			if (array_key_exists('type', $role[$key])) {
 				$this->assertEquals($dbRow['type'], $role[$key]['type']);
 			}
 		}
@@ -141,66 +155,80 @@ class testUserRole extends CAPITest {
 
 	public static function getUserRoleUpdateData() {
 		return [
-			[
-				'role' => [[
-					'roleid' => '1',
-					'name' => 'API update with non existent parameter',
-					'_nonexist' => 42
-				]],
+			'Unknown param' => [
+				'role' => [
+					[
+						'roleid' => '1',
+						'name' => 'API update with non existent parameter',
+						'_nonexist' => 42
+					]
+				],
 				'expected_error' => 'Invalid parameter "/1": unexpected parameter "_nonexist".'
 			],
-			// Check user group id.
-			[
-				'role' => [[
-					'name' => 'API user role updated'
-				]],
+			'roleid missing' => [
+				'role' => [
+					[
+						'name' => 'API user role updated'
+					]
+				],
 				'expected_error' => 'Invalid parameter "/1": the parameter "roleid" is missing.'
 			],
-			[
-				'role' => [[
-					'roleid' => '',
-					'name' => 'API user role udated'
-				]],
+			'roleid empty' => [
+				'role' => [
+					[
+						'roleid' => '',
+						'name' => 'API user role udated'
+					]
+				],
 				'expected_error' => 'Invalid parameter "/1/roleid": a number is expected.'
 			],
-			[
-				'role' => [[
-					'roleid' => '123456',
-					'name' => 'API user role udated'
-				]],
+			'Non-existing roleid' => [
+				'role' => [
+					[
+						'roleid' => '123456',
+						'name' => 'API user role udated'
+					]
+				],
 				'expected_error' => 'No permissions to referred object or it does not exist!'
 			],
-			[
-				'role' => [[
-					'roleid' => 'abc',
-					'name' => 'API user role updated'
-				]],
+			'Non-numeric roleid' => [
+				'role' => [
+					[
+						'roleid' => 'abc',
+						'name' => 'API user role updated'
+					]
+				],
 				'expected_error' => 'Invalid parameter "/1/roleid": a number is expected.'
 			],
-			[
-				'role' => [[
-					'roleid' => '1.1',
-					'name' => 'API user role udated'
-				]],
+			'Non-int roleid' => [
+				'role' => [
+					[
+						'roleid' => '1.1',
+						'name' => 'API user role udated'
+					]
+				],
 				'expected_error' => 'Invalid parameter "/1/roleid": a number is expected.'
 			],
-			// Check role group name.
-			[
-				'role' => [[
-					'roleid' => '1',
-					'name' => ''
-				]],
+			'Empty name' => [
+				'role' => [
+					[
+						'roleid' => '1',
+						'name' => ''
+					]
+				],
 				'expected_error' => 'Invalid parameter "/1/name": cannot be empty.'
 			],
-			[
-				'role' => [[
-					'roleid' => '1',
-					'name' => 'Admin role'
-				]],
+			'Update to pre-existing name' => [
+				'role' => [
+					[
+						'roleid' => '1',
+						'name' => 'Admin role',
+						'type' => USER_TYPE_ZABBIX_ADMIN
+					]
+				],
 				'expected_error' => 'User role with name "Admin role" already exists.'
 			],
-			// Check two user roles for update.
-			[
+			'Pre-existing name in set' => [
 				'role' => [
 					[
 						'roleid' => '1',
@@ -208,12 +236,12 @@ class testUserRole extends CAPITest {
 					],
 					[
 						'roleid' => '1',
-						'name' => 'User role with the same names'
+						'name' => 'Admin role'
 					]
 				],
-				'expected_error' => 'Invalid parameter "/2": value (name)=(User role with the same names) already exists.'
+				'expected_error' => 'User role with name "Admin role" already exists.'
 			],
-			[
+			'Update to same name' => [
 				'role' => [
 					[
 						'roleid' => '1',
@@ -224,18 +252,18 @@ class testUserRole extends CAPITest {
 						'name' => 'Update on user with same name'
 					]
 				],
-				'expected_error' => null
+				'expected_error' => 'Invalid parameter "/2": value (name)=(Update on user with same name) already exists.'
 			],
-			[
+			'Type change, no UIrule' => [
 				'role' => [
 					[
 						'roleid' => '1',
-						'type' => USER_TYPE_ZABBIX_ADMIN,
+						'type' => USER_TYPE_ZABBIX_ADMIN
 					],
 				],
 				'expected_error' => 'At least one UI element must be checked.'
 			],
-			[
+			'Empty ruleset' => [
 				'role' => [
 					[
 						'roleid' => '1',
@@ -245,7 +273,7 @@ class testUserRole extends CAPITest {
 				],
 				'expected_error' => 'Invalid parameter "/1/rules": an array is expected.'
 			],
-			[
+			'Non-array ruleset' => [
 				'role' => [
 					[
 						'roleid' => '1',
@@ -255,7 +283,7 @@ class testUserRole extends CAPITest {
 				],
 				'expected_error' => 'Invalid parameter "/1/rules": an array is expected.'
 			],
-			[
+			'Uknown rule' => [
 				'role' => [
 					[
 						'roleid' => '1',
@@ -265,7 +293,7 @@ class testUserRole extends CAPITest {
 				],
 				'expected_error' => 'Invalid parameter "/1/rules": unexpected parameter "_nonexist".'
 			],
-			[
+			'Name in UTF8' => [
 				'role' => [
 					[
 						'roleid' => '1',
@@ -274,23 +302,33 @@ class testUserRole extends CAPITest {
 				],
 				'expected_error' => null
 			],
-			[
+			'Update on role with no UI rules' => [
 				'role' => [
 					[
 						'roleid' => 21,
 						'type' => USER_TYPE_ZABBIX_USER,
-						'rules' => [
-						]
+						'rules' => []
 					],
 				],
 				'expected_error' => 'At least one UI element must be checked.'
 			],
-			[
+			'Update with rule on same type' => [
 				'role' => [
 					[
-						'roleid' => 21,
-						'type' => USER_TYPE_ZABBIX_USER,
-						'name' => 'User with default UI access',
+						'roleid' => 1,
+						'type' => USER_TYPE_ZABBIX_ADMIN,
+						'rules' => [
+							'ui.default_access' => 1
+						]
+					],
+				],
+				'expected_error' => null
+			],
+			'Update with rule on type change' => [
+				'role' => [
+					[
+						'roleid' => 1,
+						'type' => USER_TYPE_ZABBIX_ADMIN,
 						'rules' => [
 							'ui.default_access' => 1
 						]
@@ -304,34 +342,21 @@ class testUserRole extends CAPITest {
 	/**
 	* @dataProvider getUserRoleUpdateData
 	*/
-	public function testUserRole_Update($groups, $expected_error) {
-		$result = $this->call('role.update', $groups, $expected_error);
+	public function testUserRole_Update($role, $expected_error) {
+		$result = $this->call('role.update', $role, $expected_error);
 
-		if ($expected_error === null) {
-			foreach ($result['result']['roleids'] as $key => $id) {
-				$dbResult = DBSelect('select * from usrgrp where roleid='.zbx_dbstr($id));
-				$dbRow = DBFetch($dbResult);
-				$this->assertEquals($dbRow['name'], $groups[$key]['name']);
-				$this->assertEquals($dbRow['gui_access'], GROUP_GUI_ACCESS_SYSTEM);
-				$this->assertEquals($dbRow['users_status'], 0);
-				$this->assertEquals($dbRow['debug_mode'], 0);
-
-				if (array_key_exists('rights', $groups[$key])){
-					foreach ($groups[$key]['rights'] as $rights) {
-						$dbRight = DBSelect('select * from rights where groupid='.zbx_dbstr($id));
-						$dbRowRight = DBFetch($dbRight);
-						$this->assertEquals($dbRowRight['id'], $rights['id']);
-						$this->assertEquals($dbRowRight['permission'], $rights['permission']);
-					}
-				}
-			}
+		if ($expected_error !== null) {
+			return;
 		}
-		else {
-			foreach ($groups as $group) {
-				if (array_key_exists('name', $group) && $group['name'] != 'Zabbix administrators'){
-					$dbResult = 'select * from usrgrp where name='.zbx_dbstr($group['name']);
-					$this->assertEquals(0, CDBHelper::getCount($dbResult));
-				}
+		foreach ($result['result']['roleids'] as $key => $id) {
+			$dbResult = DBSelect('SELECT * FROM role WHERE roleid='.zbx_dbstr($id));
+			$dbRow = DBFetch($dbResult);
+
+			if (array_key_exists('name', $role[$key])) {
+				$this->assertEquals($dbRow['name'], $role[$key]['name']);
+			}
+			if (array_key_exists('type', $role[$key])) {
+				$this->assertEquals($dbRow['type'], $role[$key]['type']);
 			}
 		}
 	}
