8a6f0af655c347d62507cb5e74093e4937b41cc2
Alexander Vladishev <aleksander.vladishev@zabbix.com>
2021-07-26 08:45:45 +0300

    .......... [ZBX-19167] fixed runtime errors

--- ui/tests/include/web/CPage.php
+++ ui/tests/include/web/CPage.php
@@ -126,7 +126,7 @@ class CPage {
 		$this->resetViewport();
 
 		if (self::$cookie !== null) {
-			$session_id = $this->driver->manage()->getCookieNamed('zbx_session');
+			$session_id = $this->driver->manage()->getCookieNamed('zbx_session')->toArray();
 
 			if ($session_id === null || !array_key_exists('value', $session_id)
 					|| $session_id['value'] !== self::$cookie['value']) {
